903709d5bff8d24e3f3d87640c311d86353efb9a
JilinZhao <45322461+JilinZhao@users.noreply.github.com>
2021-01-14 16:32:52 +0800

    Delete readgroundtruth.py

--- readgroundtruth.py
+++
@@ -1,8 +0,0 @@
-a = {}
-i = 0
-for rawline in open("./groundtruth_rect.txt"):
-    line = rawline.strip()                      #去除空格和换行符
-    a[i] = line.split(',')                      #以,为分隔符分割每行字符串
-    print(a[i])
-    i += 1
-print('共有' + str(i) + '帧')
